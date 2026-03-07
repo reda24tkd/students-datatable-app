fd0352309fede47467c703b4747c8996cd9a46a4
reda24tkd <reda2004benabbou@gmail.com>
2026-03-07 15:59:04 +0000

    railway db conf env

--- src/main/java/ensa/tps/_4_jsf_datatable/DatabaseConnection.java
+++ src/main/java/ensa/tps/_4_jsf_datatable/DatabaseConnection.java
@@ -7,14 +7,20 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static DatabaseConnection instance;
     private Connection connection;
-    private static final String URL = "jdbc:mysql://localhost:3306/railway";
-    private static final String USER = "root";
-    private static final String PASSWORD = "RpXNgcTQgXmkGNMJolIMnMtHBxdNLhRS";
+//    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "";
+    String host = System.getenv("MYSQL_HOST");
+    String port = System.getenv("MYSQL_PORT");
+    String db   = System.getenv("MYSQL_DATABASE");
+    String user = System.getenv("MYSQL_USER");
+    String pass = System.getenv("MYSQL_PASSWORD");
 
+    String url = "jdbc:mysql://" + host + ":" + port + "/" + db;
     private DatabaseConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
